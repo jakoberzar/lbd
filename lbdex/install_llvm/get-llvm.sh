bb179d54d70176865e841ea7a5ff5d05cbaf1460
Jakob Erzar <jakob.erzar@gmail.com>
2018-06-28 19:58:58 +0200

    Updated LLVM download url.
    The old url is behind a redirect and curl without the -L option fails.
    Instead of redirecting I updated the URL to the correct one.

--- lbdex/install_llvm/get-llvm.sh
+++ lbdex/install_llvm/get-llvm.sh
@@ -2,8 +2,7 @@
 
 VERSION=3.9.0
 
-# Download address can be gotten from "Copy link location" of right clicking 
+# Download address can be gotten from "Copy link location" of right clicking
 # mouse on firefox browser on llvm.org download page.
-curl -O http://llvm.org/releases/${VERSION}/llvm-${VERSION}.src.tar.xz
-curl -O http://llvm.org/releases/${VERSION}/cfe-${VERSION}.src.tar.xz
-
+curl -O http://releases.llvm.org/${VERSION}/llvm-${VERSION}.src.tar.xz
+curl -O http://releases.llvm.org/${VERSION}/cfe-${VERSION}.src.tar.xz
